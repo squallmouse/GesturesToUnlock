c6e2ce6fc1f1698261aaf8819aafcdb81e0c4e23
squallmouse <yuanhao188@sohu.com>
2015-10-09 16:10:56 +0800

    no message

---
+++ GesturesToUnlock/GesturesToUnlock/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GesturesToUnlock
+//
+//  Created by 袁昊 on 15/10/9.
+//  Copyright © 2015年 squallmouse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

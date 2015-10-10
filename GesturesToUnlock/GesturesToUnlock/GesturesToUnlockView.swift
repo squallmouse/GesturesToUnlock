fd86a2ba30f0268aa1bd482c3e2e229842aa5c41
squallmouse <yuanhao188@sohu.com>
2015-10-10 14:30:17 +0800

    s

---
+++ GesturesToUnlock/GesturesToUnlock/GesturesToUnlockView.swift
@@ -0,0 +1,21 @@
+//
+//  GesturesToUnlockView.swift
+//  GesturesToUnlock
+//
+//  Created by 袁昊 on 15/10/9.
+//  Copyright © 2015年 squallmouse. All rights reserved.
+//
+
+import UIKit
+
+class GesturesToUnlockView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
